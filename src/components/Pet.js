656edf443df02d03eae2b558e942ff195baabc59
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-06-13 14:19:41 -0500

    feature branch close; about section complete

---
+++ src/components/Pet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Pet = ()=>{
+    return(
+        //react.createElemnt()
+        <div>
+            <h1>Ginger</h1>
+            <p>breed: Brittany Spaniel</p>
+        </div>
+    )
+}
+
+export default Pet;
